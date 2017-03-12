6b5533f8b2aa68a3879df550c70a13e3859bbc45
sutantowilliam <willywilliamss97@gmail.com>
2017-03-13 02:18:35 +0700

    Animal.cpp

---
+++ Cell.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "other.h"
+
+class Cell {
+public:
+	/** @brief Constructor
+	*/
+	Cell();
+
+	/** @brief Constructor
+	  * @param  _type tipe sel 
+	  * @param _id karakter id sel
+	  * @param _color warna sel
+	  * @param _m baris
+	  * @param _n kolom
+	  */
+	Cell(Place _type, char _id,Color _color, int _m, int _n);
+	
+	/** @brief Copy Constructor
+	* @param Cell C yang ingin disalin.
+	*/
+	Cell(const Cell& C);
+
+	/** @brief Destructor.
+    */
+	virtual ~Cell();
+
+	/** @brief Operator=
+	  * Menjamin bukan bitwise copy.
+	  * @return Animal yang sudah di assign nilai dari current object.
+	  */
+	Cell& operator=(const Cell& C);
+
+	//friend istream& operator>>(istream& in, Animal& A);
+
+private:
+	Place type;
+	char id;
+	Color color;
+	int m;
+	int n;
+};
